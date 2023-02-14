2b8520ad2b2d85faf8ae3f9460f705d0a3e1170c
daudujohn <daudujoe@gmail.com>
2023-02-14 10:50:56 +0100

    Added config

--- 5. Express- Advanced Topics/index.js
+++ 5. Express- Advanced Topics/index.js
@@ -1,13 +1,28 @@
+const config = require('config')
 const Joi = require('joi');
+const logger = require("./logger")
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+
 app.use(express.json());
+app.use(logger);
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(helmet());
 
-app.use((req, res, next) => {
-    console.log('Logging');
-    next();
-})
+// Configuration
+console.log(`Application Name: ${config.get('name')}`)
+console.log(`Mail Server: ${config.get('mail.host')}`)
+console.log(`Mail password: ${config.get('mail.password')}`)
 
+if (app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...')
+    console.log(app.get('env'))
+}
 
 const courses = [
     {id: 1, name: 'course1'},
