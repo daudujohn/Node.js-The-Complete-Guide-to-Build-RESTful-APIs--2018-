e83dbf7b2eebf795e183b92f9b832d715d49f7e3
daudujohn <daudujoe@gmail.com>
2023-02-22 12:47:41 +0100

    Added countCourses function

--- 7. CRUD Operations Using Mongoose/mongo-demo/index.js
+++ 7. CRUD Operations Using Mongoose/mongo-demo/index.js
@@ -13,17 +13,40 @@ const courseSchema = new mongoose.Schema({
     isPublished: Boolean
 })
 
+const Course = mongoose.model('Course', courseSchema);
+
 async function createCourse(){
-    const Course = mongoose.model('Course', courseSchema);
     const course = new Course({
         name: 'React Course', 
         author: 'Mosh', 
         'tags': ['react', 'frontend'], 
-        isPusblished: true
+        isPubslished: true
     })
     
     const result = await course.save();
     console.log(result)
 }
 
-createCourse();
+async function getCourses(){
+    const courses = await Course
+    .find({author: 'Mosh'})
+    .limit(10)
+    .sort({name: 1})
+    .select({name: 1, tags: 1});
+    console.log(courses);
+}
+
+async function countCourses(){
+    const count = await Course
+    .find({ $or: [{tags: {$in: ['frontend']}}]})
+    // .or([{tags: ['backend']}, {tags: ['end']}])
+    .limit(10)
+    .sort({name: 1})
+    .countDocuments()
+
+    console.log(count);
+}
+countCourses();
+
+
+// getCourses();
