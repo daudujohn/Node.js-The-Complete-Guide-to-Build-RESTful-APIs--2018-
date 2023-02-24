f932765be63f89d050a0d57c5fd3dcdfdad50be2
daudujohn <daudujoe@gmail.com>
2023-02-24 12:23:47 +0100

    Created customer.js

---
+++ vidly/models/customer.js
@@ -0,0 +1,32 @@
+const Joi = require('joi')
+const mongoose = require('mongoose')
+
+
+const customerSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        minlength: 3, 
+        maxlength: 255, 
+        trim: true, 
+        required: true
+    }, 
+    isGold: {
+        type: Boolean, 
+        required: false
+    }, 
+    phone: {
+        type: String, 
+         minlength: 8, 
+        maxlength: 20, 
+        trim: true, 
+        required: true
+    }
+})
+
+
+function validateCustomer(schema, customer){
+    return schema.validate(customer)
+}
+
+exports.Customer = Customer;
+exports.validateCustomer = validateCustomer;
