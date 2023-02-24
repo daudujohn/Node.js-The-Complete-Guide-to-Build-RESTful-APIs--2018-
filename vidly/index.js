43060e88069dcff4d19171d8866c3a5ea1c504e2
daudujohn <daudujoe@gmail.com>
2023-02-24 04:06:07 +0100

    Added customer api middleware route

--- vidly/index.js
+++ vidly/index.js
@@ -1,10 +1,18 @@
+const config = require('config');
+const mongoose = require('mongoose');
 const Joi = require('joi');
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
+
+mongoose.connect(config.get('database.conn_string'))
+    .then(console.log('Connected to MongoDB...'))
+    .catch((err) => console.error("Error:", err.message))
 
 app.use(express.json());
 app.use('/api/genres', genres);
+app.use('/api/customers', customers)
 
 
 const port = process.env.PORT || 3000
