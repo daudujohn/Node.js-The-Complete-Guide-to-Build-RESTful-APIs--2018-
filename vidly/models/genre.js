993578edd44ffdd695536c179f46d524b29d7395
daudujohn <daudujoe@gmail.com>
2023-02-25 17:58:47 +0100

    exported genreSchema

---
+++ vidly/models/genre.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Joi = require('joi')
+
+// create a valid schema
+const genreSchema = mongoose.Schema({
+    name: {
+        type: String, 
+        required: true, 
+        minlength: 3, 
+        maxlength: 50, 
+        validate: {
+            isAsync: true, 
+            validator: async function(value) {
+              const existingGenre = await mongoose.model('Genre').findOne({ name: value });
+              return !existingGenre;
+            },
+            message: 'Genre already exists',
+        },
+        trim: true, 
+    }
+})
+// create a model
+const Genre = mongoose.model('Genre', genreSchema);
+
+function validateGenre(schema, genre) {
+    return schema.validate(genre);
+}
+
+exports.Genre = Genre;
+exports.validateGenre = validateGenre;
+exports.genreSchema = genreSchema;
