1206d88c54a971016f4c77f3da57dbf0a384717b
daudujohn <daudujoe@gmail.com>
2023-02-25 18:00:20 +0100

    Added movies middleware

--- vidly/index.js
+++ vidly/index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies')
 
 mongoose.connect(config.get('database.conn_string'))
     .then(console.log('Connected to MongoDB...'))
@@ -13,6 +14,7 @@ mongoose.connect(config.get('database.conn_string'))
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers)
+app.use('/api/movies', movies)
 
 
 const port = process.env.PORT || 3000
