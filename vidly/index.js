9a76791d9bf8b677a11e19241a0a4e3f720b50a0
daudujohn <daudujoe@gmail.com>
2023-02-28 02:23:23 +0100

    Added rental middleware

--- vidly/index.js
+++ vidly/index.js
@@ -6,6 +6,7 @@ const app = express();
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies')
+const rentals = require('./routes/rentals')
 
 mongoose.connect(config.get('database.conn_string'))
     .then(console.log('Connected to MongoDB...'))
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers)
 app.use('/api/movies', movies)
+app.use('/api/rentals', rentals)
 
 
 const port = process.env.PORT || 3000
