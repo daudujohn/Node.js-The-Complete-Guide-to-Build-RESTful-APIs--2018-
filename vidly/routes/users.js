8650d8ec31189dba4fbf01e5f78076c4df255a27
daudujohn <daudujoe@gmail.com>
2023-03-04 12:43:02 +0100

    updated token generation method

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
 const {User, validateUser} = require('../models/user');
 const Joi = require('joi')
 const express = require('express');
@@ -40,14 +42,15 @@ router.post('/', async(req, res) => {
         let user = await User.findOne({email: req.body.email})
         if (user) return res.status(400).send('User already registered.');
 
-        user = new User({
-            name: req.body.name, 
-            email: req.body.email, 
-            password: req.body.password, 
-        })
-    
+        user = new User(_.pick(req.body, ['name', 'email', 'password']))
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt)
+
         await user.save();
-        res.send(user)
+
+        const token = user.generateAuthToken();
+        res.header("x-auth-token", token).send(_.pick(user, ['_id', 'name', 'email']))
     }
     catch(ex) {
         for (field in ex.errors){
@@ -62,11 +65,7 @@ router.put('/:id', async(req, res) => {
     const {error} = validateMovie(req.body)
     if (error) return res.status(400).send(error.details[0].message)
 
-    const updatedUser = {
-        name: req.body.name, 
-        email: req.body.email, 
-        password: req.body.password, 
-    }
+    const updatedUser = _.pick(req.body, ['name', 'email', 'password'])
     
     const user = await User.findByIdAndUpdate(req.params.id, updatedUser, {new: true})
     if (!user) return res.status(400).send('User does not exist')
