0226fb43ec4ffc1659a054edb3993ed010ca6e3a
daudujohn <daudujoe@gmail.com>
2023-03-04 12:44:20 +0100

    Added control flow to ensure private key is set

--- vidly/index.js
+++ vidly/index.js
@@ -9,6 +9,12 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies')
 const rentals = require('./routes/rentals')
 const users = require('./routes/users');
+const auth = require('./routes/auth')
+
+if (!config.get('jwtPrivateKey')){
+    console.error("FATAL ERROR: jwtPrivateKey is not defined.");
+    process.exit(1);
+}
 
 mongoose.connect(config.get('database.conn_string'))
     .then(console.log('Connected to MongoDB...'))
@@ -20,6 +26,7 @@ app.use('/api/customers', customers)
 app.use('/api/movies', movies)
 app.use('/api/rentals', rentals)
 app.use('/api/users', users)
+app.use('/api/auth', auth)
 
 
 const port = process.env.PORT || 3000
