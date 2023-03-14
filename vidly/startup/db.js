9afebecf34ec48cb09aa768ca3f4b0a46e9e81d6
daudujohn <daudujoe@gmail.com>
2023-03-14 03:28:20 +0100

    Update db.js
    Set useUnifiedTopology option to true

--- vidly/startup/db.js
+++ vidly/startup/db.js
@@ -4,6 +4,6 @@ const winston = require('winston');
 
 module.exports = function () {
     const db = config.get('database.conn_string');
-    mongoose.connect(db)
+    mongoose.connect(db, {'useUnifiedTopology': true})
         .then(() => {winston.info(`Connected to ${db}...`)})
 }
